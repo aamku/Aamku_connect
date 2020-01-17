c92a7da18d18eba86b302c65267d71bafe6fa75d
aamku <Aamku@lifestyle1>
2020-01-17 12:32:42 +0530

    Action bar hide

--- app/src/main/java/com/example/aamkuconnect/MainActivity.java
+++ app/src/main/java/com/example/aamkuconnect/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.aamkuconnect;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar ab = getSupportActionBar();
+        assert ab != null;
+        ab.hide();
+
     }
 }
